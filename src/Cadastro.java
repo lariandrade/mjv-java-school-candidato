ece4628c90f09dee4af152dd0f9e6e33e2a15db4
Larissa Andrade <larissa.sandrade@hotmail.com>
2023-06-16 20:50:07 -0300

    feat: criar atributos da classe cadastro

---
+++ src/Cadastro.java
@@ -0,0 +1,157 @@
+import java.time.LocalDate;
+
+public class Cadastro {
+
+    private String nome; 
+    private String CPF;
+    private LocalDate dataNascimento;
+    private SexoEnum sexo;
+    private String logradouro;
+    private String numero; 
+    private String bairro; 
+    private String complemento; 
+    private String cidade;
+    private String estado;
+    private String email;
+    private Long telefone;
+    private Long celular;
+    private boolean celularWhats;
+    private String profissao;
+    private String empresa;
+    private Double salario;
+    private boolean empregoAtual;
+    private Double pretencaoMinima;
+    private Double pretencaoMaxima;
+    private String habilidades;
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getCPF() {
+        return CPF;
+    }
+    public void setCPF(String cPF) {
+        CPF = cPF;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    } 
+
+    public SexoEnum getSexo() {
+        return sexo;
+    }
+    public void setSexo(SexoEnum sexo) {
+        this.sexo = sexo;
+    }
+    public String getLogradouro() {
+        return logradouro;
+    }
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+    public String getNumero() {
+        return numero;
+    }
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+    public String getBairro() {
+        return bairro;
+    }
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+    public String getComplemento() {
+        return complemento;
+    }
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+    public String getCidade() {
+        return cidade;
+    }
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+    public String getEstado() {
+        return estado;
+    }
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public Long getTelefone() {
+        return telefone;
+    }
+    public void setTelefone(Long telefone) {
+        this.telefone = telefone;
+    }
+    public Long getCelular() {
+        return celular;
+    }
+    public void setCelular(Long celular) {
+        this.celular = celular;
+    }
+    public boolean isCelularWhats() {
+        return celularWhats;
+    }
+    public void setCelularWhats(boolean celularWhats) {
+        this.celularWhats = celularWhats;
+    }
+    public String getProfissao() {
+        return profissao;
+    }
+    public void setProfissao(String profissao) {
+        this.profissao = profissao;
+    }
+    public String getEmpresa() {
+        return empresa;
+    }
+    public void setEmpresa(String empresa) {
+        this.empresa = empresa;
+    }
+    public Double getSalario() {
+        return salario;
+    }
+    public void setSalario(Double salario) {
+        this.salario = salario;
+    }
+    public boolean isEmpregoAtual() {
+        return empregoAtual;
+    }
+    public void setEmpregoAtual(boolean empregoAtual) {
+        this.empregoAtual = empregoAtual;
+    }
+    public Double getPretencaoMinima() {
+        return pretencaoMinima;
+    }
+    public void setPretencaoMinima(Double pretencaoMinima) {
+        this.pretencaoMinima = pretencaoMinima;
+    }
+    public Double getPretencaoMaxima() {
+        return pretencaoMaxima;
+    }
+    public void setPretencaoMaxima(Double pretencaoMaxima) {
+        this.pretencaoMaxima = pretencaoMaxima;
+    }
+    public String getHabilidades() {
+        return habilidades;
+    }
+    public void setHabilidades(String habilidades) {
+        this.habilidades = habilidades;
+    }
+
+
+}
